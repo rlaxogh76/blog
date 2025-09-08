1f358db80225e2efc65ca036c03061a115ff5b2f
rlaxogh76 <btm.email2769@gmail.com>
2025-09-08 22:51:32 +0900

    feat: Project 페이지 구현

---
+++ src/pages/Project/Project.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/Project.css";
+const Blog: React.FC = () => {
+  return (
+    <div style={{ color: "#222", textAlign: "center", marginTop: 50 }}>
+      <h2 className="text">프로젝트 페이지</h2>
+      <p className="text">여기에 프로젝트 글 목록이나 내용을 추가하세요.</p>
+    </div>
+  );
+};
+
+export default Blog;
