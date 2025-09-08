61b51af8f702e3966549232ec23bbc9a7a0cb4de
rlaxogh76 <btm.email2769@gmail.com>
2025-09-08 22:48:37 +0900

    feat: 페이지 라우터 추가 및 box css 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import type React from "react";
 import "./App.css";
 import Header from "./components/Layout/Header";
+import { Routes, Route } from "react-router-dom";
+import Blog from "./pages/Blog/Blog";
+import Project from "./pages/Project/Project";
+
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <div className="hero-text">
+                <h1>Hello world</h1>
+              </div>
+              <div
+                className="box"
+                style={{ "--color": "orange" } as React.CSSProperties}
+              ></div>
+              <div
+                className="box"
+                style={{ "--color": "cyan" } as React.CSSProperties}
+              ></div>
+            </div>
+          }
+        />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/Project" element={<Project />} />
+      </Routes>
     </>
   );
 }
