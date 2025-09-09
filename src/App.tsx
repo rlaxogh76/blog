f3a168d3a876c73363d9416bb073c9441fa17614
rlaxogh76 <btm.email2769@gmail.com>
2025-09-09 19:56:45 +0900

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,41 +16,42 @@ function App() {
           path="/"
           element={
             <div className="scroll-container">
-              <div className="scroll-area hero-text title">
+              <div className="hero-text title">
                 <h1 className="title">
                   rlaxogh
                   <span style={{ color: "#4979ff" }}>76</span>
                 </h1>
               </div>
-              <div className="line"></div>
+              {/* <div className="line"></div> */}
+
+              <h1 className="main-text">인기 글</h1>
               <div
-                className="box scroll-area"
+                className="box section-box-1"
+                style={{ "--color": "white" } as React.CSSProperties}
+              ></div>
+              <div
+                className="box"
                 style={{ "--color": "orange" } as React.CSSProperties}
               ></div>
               <div
-                className="box scroll-area"
+                className="box"
                 style={{ "--color": "cyan" } as React.CSSProperties}
               ></div>
               <div
-                className="box scroll-area"
+                className="box"
                 style={{ "--color": "orange" } as React.CSSProperties}
               ></div>
               <div
-                className="box scroll-area"
-                style={{ "--color": "cyan" } as React.CSSProperties}
-              ></div>
-              <div
-                className="box scroll-area"
+                className="box"
                 style={{ "--color": "orange" } as React.CSSProperties}
               ></div>
-              <div
-                className="box scroll-area"
-                style={{ "--color": "orange" } as React.CSSProperties}
-              ></div>
-              <div
-                className="box scroll-area"
-                style={{ "--color": "cyan" } as React.CSSProperties}
-              ></div>
+              <div className="Github-link">
+                <img
+                  src="https://avatars.githubusercontent.com/u/108007761?v=4"
+                  className="Github-img"
+                  alt="Github image"
+                />
+              </div>
               <Footer />
             </div>
           }
--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,17 +1,33 @@
 import "../../styles/Layout/Footer.css";
-import bmcLogo from "../../assets/bmc-logo.svg";
+import gmail from "../../assets/gmail.svg";
+// import bmcLogo from "../../assets/bmc-logo.svg";
+import github from "../../assets/github.svg";
 
 const Footer: React.FC = () => {
   return (
     <footer className="custom-footer">
-      <p>© 2025 rlaxogh76. All rights reserved.</p>
-      <a
-        href="https://www.buymeacoffee.com/rlaxogh76"
-        target="_blank"
-        rel="noopener noreferrer"
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
       >
-        <img src={bmcLogo} className="footer-img" alt="Buy Me a Coffee logo" />
-      </a>
+        <div>
+          <img src={gmail} className="footer-icon" alt="footer image" />
+          <img src={github} className="footer-icon" alt="footer image" />
+        </div>
+        <p style={{ margin: "8px 0 0 0" }}>
+          © 2025 rlaxogh76. All rights reserved.
+        </p>
+        <a
+          href="https://www.buymeacoffee.com/rlaxogh76"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {/* <img src={bmcLogo} className="footer-icon" alt="Buy Me a Coffee logo" /> */}
+        </a>
+      </div>
     </footer>
   );
 };
